a8fc41aba21f5fb3a326cf0730d12bdbbf5c0014
cicero Johnny <cicerojohnny@localhost.localdomain>
2020-11-05 20:01:39 -0300

    iniciando os trabalhos

---
+++ urna/etapas.js
@@ -0,0 +1,58 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos:[
+            {   
+                numero: '33222',
+                nome: 'Fulano de Tal',
+                partido: 'ABC',
+                fotos:[
+                    {url:"33222.png", legenda: "Vereador" ,  small: false}
+                ]
+            },
+            {
+                numero:'44555',
+                nome:'Beltrano da silva',
+                partido: "DEFG",
+                fotos:[
+                    {url:"44555.png", legenda: "Vereador"}
+                ]
+             },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos:[
+            {
+                numero: '00',
+                nome: 'Ciclano',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos:[
+                    {url:"00.png",legenda:"Prefeito"},
+                    {url:"33.jpg",legenda: "Vice-Prefeito", small: true}
+                ]
+            },
+            {
+                numero: '66',
+                nome: 'Zulano',
+                partido: 'QWERTY',
+                vice: 'Zul',
+                fotos:[
+                    {url:"66.png",legenda:"Prefeito"},
+                    {url:"22.png",legenda:"Vice-Prefeito", small: true}
+                ]
+            },
+        
+        ]
+
+    },
+];
+
+
+
+
+
+
